0bac58baccfa00796e0178ff0e72d250762dcb49
Zoey Lee <zoeylee02@gmail.com>
2026-05-01 17:36:44 +0200

    Add autori solution in Go

---
+++ go/autori/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    var x string
+
+    fmt.Scanln(&x)
+
+    parts := strings.Split(x, "-")
+
+    Initials := ""
+
+    for _, part := range parts {
+      a := string(part[0])
+      Initials += a
+    }
+
+    fmt.Println(Initials)
+}
