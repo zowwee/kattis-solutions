1f4877bc9fbf8a5eadc5a794ac4a0485725d20d5
Zoey Lee <zoeylee02@gmail.com>
2026-05-01 19:09:41 +0200

    Add quadrant-selector solution in python

---
+++ python/quadrant-selector.py
@@ -0,0 +1,14 @@
+first = int(input())
+second = int(input())
+
+if first > 0 and second > 0:
+    print("1")
+
+elif first < 0 and second > 0:
+    print("2")
+
+elif first < 0 and second < 0:
+    print("3")
+
+else:
+    print("4")
