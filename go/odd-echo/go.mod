cf91e1c94f656c66466fca6d9414715aaf214340
Zoey Lee <zoeylee02@gmail.com>
2026-05-01 17:25:04 +0200

    Add solution for odd echo problem

---
+++ go/odd-echo/go.mod
@@ -0,0 +1,3 @@
+module odd-echo
+
+go 1.25.7
---
+++ go/odd-echo/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+    var n int
+
+    fmt.Scanln(&n)
+
+    for i := 1; i < n +1; i ++ {
+        var x string
+        fmt.Scanln(&x)
+
+        if i%2 != 0 {
+            fmt.Println(x)
+        }
+    }
