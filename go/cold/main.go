594d7bbdf57b21997a44fd8d307dc3630b15da9c
Zoey Lee <zoeylee02@gmail.com>
2026-05-01 17:35:08 +0200

    Add cold problem in Go

---
+++ go/cold/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+    var n int
+    fmt.Scanln(&n)
+
+
+    sum := 0
+
+    for i := 0; i < n; i++ {
+        var x int
+        fmt.Scan(&x)
+
+        if x < 0 {
+            sum ++
+        }
+
+    }
+
+
+    fmt.Println(sum)
+}
